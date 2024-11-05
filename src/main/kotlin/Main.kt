d1dcdaaf03d0b7123b97b5acd1aab0b178e55f26
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-05 10:51:11 -0300

    treino kotlin em aula

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,18 +4,23 @@ fun main() {
     no javascript*/
 
     /*consigo reatribuir o valor na variável que declaro como 'var'*/
-    var resultado = 10 + 1;
+    var resultado: Int = 10 + 1;
     println(resultado);
     resultado = 10+10;
     println(resultado);
     println("")
 
     /*já quando declaro a variável como 'val' o valor é imutável, não posso fazer reatribuição*/
-    val novoResultado = 10+30;
+    val novoResultado: Int = 10+30;
     println(novoResultado::class); // para saber o tipo de dado armazenado basta usar '::class'
     //novoResultado = 20; (vai dar erro)
+    println("")
 
-    val nome = "Carlos";
+    val nome: String = "Carlos"; //tipo explícito
     println(nome::class);
+    println("")
+
+    val sobrenome = "Teixeira"; //tipo inferido
+    println(sobrenome::class);
 
 }
