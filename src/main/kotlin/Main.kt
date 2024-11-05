ed0f02551ddebf0db049610c9db640b801513aea
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-04 21:26:29 -0300

    treino kotlin em aula

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,17 @@ fun main() {
     // é assim que se comenta no kotlin
     /*é a mesma coisa que
     no javascript*/
-    
+
+    /*consigo reatribuir o valor na variável que declaro como 'var'*/
     var resultado = 10 + 1;
-    print(resultado);
+    println(resultado);
+    resultado = 10+10;
+    println(resultado);
+    println("")
+
+    /*já quando declaro a variável como 'val' o valor é imutável, não posso fazer reatribuição*/
+    val novoResultado = 10+30;
+    println(novoResultado);
+    //novoResultado = 20; (vai dar erro)
+
 }
