078e56e52a3847139c5c7fb0a4435687b2936879
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-04 21:13:11 -0300

    treino kotlin em aula

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 fun main() {
-    var nome = "Carlos";
-    println(nome);
-    nome = "Ingrid";
-    print(nome);
+    // é assim que se comenta no kotlin
+    /*é a mesma coisa que
+    no javascript*/
+    
+    var resultado = 10 + 1;
+    print(resultado);
 }
