cbe170337755eff12f6873d6517003213dd2ce9e
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-08 13:34:20 -0300

    treino kotlin em aula

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
 fun main() {
-    val numero = 10;
-    println(numero);
+    val n1: Int = 23;
+    val numeroString: String = "22";
+    val n2 = numeroString.toInt();
+    val respostaSoma = n1 + n2;
+    println(respostaSoma);
 }
