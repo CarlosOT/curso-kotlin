fb9ceb193fa37fae617dc6f5b9804aba32ed52af
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-08 13:22:21 -0300

    treino kotlin em aula

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,6 @@
 fun main() {
-    val nome = "Carlos"
-    val idade = 28
-    val primeiraLetra: Char = nome[0]
-    val altura = 1.69f
-    val peso = 68
-    val programador = true
-
-    println(nome)
-    println(idade)
-    println(primeiraLetra)
-    println(altura)
-    println(peso)
-    println(programador)
+    val mediaPreco = 22;
+    println(mediaPreco::class);
+    val resposta = mediaPreco.toDouble();
+    println(resposta::class)
 }
