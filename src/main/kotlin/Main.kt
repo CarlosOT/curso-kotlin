0258b8329d652306614cd36f6ae7f6a8057484cc
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-08 14:19:23 -0300

    treino kotlin em aula

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 fun main() {
-    val nome: String = "Carlos";
-    print(nome.length); //acessar a propriedade de um objeto
-    print(nome.uppercase()); //funcao que muda o comportamento do objeto
+    val texto = """
+        Olá, Carlos!
+        Seu produto chegou...
+        """.trimIndent();
+    print(texto);
 }
