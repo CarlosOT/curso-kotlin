8350268b11fc4b53e7c643a748f1910a61f5d2f1
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-08 13:29:01 -0300

    treino kotlin em aula

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,4 @@
 fun main() {
-    val mediaPreco = 22;
-    println(mediaPreco::class);
-    val resposta = mediaPreco.toDouble();
-    println(resposta::class)
+    val numero = 10;
+    println(numero);
 }
