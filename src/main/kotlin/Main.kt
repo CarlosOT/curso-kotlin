25da534a03b843097208d4f3e0b3c570e62de3b0
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-18 16:47:57 -0300

    treino kotlin em aula

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,21 @@
 fun main() {
-    var usuarioLogado = false;
+    var testPrograma = false;
 
-    while(usuarioLogado == false){
-        println("Login!")
-        print("Digite seu Usuario: ")
-        val usu = readln()
-        print("Digite sua Senha: ")
-        val senha = readln()
-        val intSenha = senha.toInt()
-        val usuData = "Felipe"
-        val senhaData = 1234
-        if (usu == usuData && intSenha == senhaData){
-            println("Login efetuado com sucesso!");
-            usuarioLogado = true;
-        }else {
-            println("Usuário ou Senha incorretos, tente novamente!");
+     while(testPrograma == false){
+         print("Insira o ID do produto: ");
+         val produtoId = readln().toInt();
+
+         when(produtoId){
+             1,3 -> {
+                 println("Você ganhou 10% de desconto");
+             }
+             2 -> {
+                 println("Você ganhou 20% de desconto");
+             }
+             else ->{
+                 println("Resposta padrão");
+                 testPrograma = true;
+             }
         }
-    }
+     }
 }
