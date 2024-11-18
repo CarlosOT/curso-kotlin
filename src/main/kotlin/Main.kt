6c5f208c1e0cdad83175dc54a8c8739ea6634435
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-18 15:19:04 -0300

    treino kotlin em aula

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    print("Primeiro Nome:"); val nome = readln();
-    print("Segundo Nome: "); val sobrenome = readln();
-    print("Nome Completo: ${nome + " " + sobrenome}");
+    val numeroString = readln();
+    val numero = numeroString.toInt();
+    print(numero::class);
 }
