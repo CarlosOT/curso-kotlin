3b852aa00510a91af6315c0cc4ed0c3cbdf44c45
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-19 14:02:57 -0300

    treino kotlin em aula

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,20 +2,11 @@ fun main() {
     var testPrograma = false;
 
      while(testPrograma == false){
-         print("Insira o ID do produto: ");
-         val produtoId = readln().toInt();
-
-         when(produtoId){
-             1,3 -> {
-                 println("Você ganhou 10% de desconto");
-             }
-             2 -> {
-                 println("Você ganhou 20% de desconto");
-             }
-             else ->{
-                 println("Resposta padrão");
-                 testPrograma = true;
-             }
+         print("Teste: ");
+         val limite = readln().toInt();
+         for(i in 0 until limite){
+             println(i);
+         }
+         testPrograma = true;
         }
-     }
 }
