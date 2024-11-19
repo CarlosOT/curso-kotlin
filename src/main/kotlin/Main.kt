a0d56cbb4c1d9f6508e438245ce1041a73bed10d
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-19 15:49:03 -0300

    treino kotlin em aula

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,3 @@
 fun main() {
-    print("Insira nome do usuário: ");
-    var nameUser: String? = readln();
-    if(nameUser?.length == 0){
-        nameUser = null;
-    }
-    val checkNameUser = nameUser ?: "usuário Desconhecido";
-    print("Seja bem Vindo(a), $checkNameUser");
+    
 }
