43c5648ecb531e6800757773336cc0e295c4cf26
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-25 11:55:25 -0300

    treino kotlin em aula

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,66 @@
 fun main(){
-    val user1 = User();
-    user1.name = "Carlos";
-    user1.password = "123";
-    println(user1.name);
-    println(user1.password);
+    val controle =  PlayerDeVideo(50);
+    controle.aumentarVolume();
+    controle.diminuirVolume();
+    controle.aumentarVolume();
+    controle.aumentarVolume();
+    controle.aumentarVolume();
+    controle.aumentarVolume();
+    controle.aumentarVolume();
+    controle.aumentarVolume();
+    controle.aumentarVolume();
+    controle.diminuirVolume();
+    controle.diminuirVolume();
+    controle.diminuirVolume();
+    controle.diminuirVolume();
+    controle.diminuirVolume();
+    controle.diminuirVolume();
+    controle.diminuirVolume();
+    controle.diminuirVolume();
+    controle.diminuirVolume();
+    controle.diminuirVolume();
+    controle.diminuirVolume();
+    controle.diminuirVolume();
+}
 
-    user1.updatePassword(user1.name);
-    println(user1.password);
+class PlayerDeVideo(private var volume: Int){
+
+    private fun validarVolume(): Boolean{
+        if (getVolume() >= 0 && getVolume() <= 100){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public fun aumentarVolume(){
+        if(validarVolume() && getVolume() != 100){
+            setVolume(getVolume() + 10);
+            println("Volume(+): ${getVolume()}");
+        }else{
+            setVolume(100);
+            println("Volume máximo(x): ${getVolume()}");
+        }
+
+    }
+
+    public fun diminuirVolume(){
+        if(validarVolume() &&getVolume() != 0){
+            setVolume(getVolume() - 10);
+            println("Volume(-): ${getVolume()}");
+        }else{
+            setVolume(0);
+            println("Volume mínimo(x): ${getVolume()}");
+        }
+    }
+
+    private fun getVolume(): Int{
+        return volume;
+    }
+
+    private fun setVolume(value: Int){
+        volume = value;
+    }
 
 }
 
